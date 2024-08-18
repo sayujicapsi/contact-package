84720fa07de51ceb4a20e68999646c390c3f82d5
sayuj xaviour <sayuj.icapsi@gmail.com>
2024-08-18 16:32:04 +0530

    modified

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Sxp\Contact\Http\Controllers\ContactController;
-Route::middleware(['web'])->group(function() {
 
-    Route::get('contact', [ContactController::class, 'view'])->name('contact.view');
-    Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
-});
 
+Route::get('contact', [ContactController::class, 'view'])->name('contact.view');
+Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
